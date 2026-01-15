84ab8d0c643753c325d873ba58f30ada3a7ac4f6
JharedAndrei <jharedandrei.tadifa@lorma.edu>
2026-01-15 12:10:19 +0800

    motorcycle

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,21 +1,21 @@
 public class Motorcycle {
-    public static void main(String[] args) {
-        Motorcycle obj1 = new Motorcycle();
-        obj1.model = "Aerox 155";
-        obj1.displacementcc = 155;
-        obj1.company = "Yamaha";
-        obj1.color = "Blue";
-        obj1.price = 150000;
 
-        Motorcycle obj2 = new Motorcycle();
-        obj2.model = "Pcx 160";
-        obj2.displacementcc = 157;
-        obj2.company = "Honda";
-        obj2.color = "Black";
-        obj2.price = 170000;
+        String model = "Aerox 155";
+       double displacementcc = 155;
+        String company = "Yamaha";
+        String color = "Blue";
+        double price = 150000;
 
-        obj1.printDetails();
-        obj2.printDetails();
+        String model2 = "Pcx 160";
+        double displacementcc2 = 157;
+        String company2 = "Honda";
+        String color2 = "Black";
+        double price2 = 170000;
+     
+
+        void printDetails(){
+            System.out.printf("Model: %s, Displacement: %.1fcc, Company: %s, Color: %s, Price: %.2f%n", model, displacementcc, company, color, price);
+            System.out.printf("Model: %s, Displacement: %.1fcc, Company: %s, Color: %s, Price: %.2f%n", model2, displacementcc2, company2, color2, price2);
+        }
 
     }
-}
